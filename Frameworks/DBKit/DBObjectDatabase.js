34908e347e66c59d92683549d196b45727f914dd
Owen Shaw <owen@owenshaw.com>
2019-04-09 10:39:15 -0700

    simple object database

---
+++ Frameworks/DBKit/DBObjectDatabase.js
@@ -0,0 +1,91 @@
+// #import Foundation
+// #import Hash
+/* global JSClass, JSObject, DBObjectDatabase, UUID, JSSHA1Hash */
+'use strict';
+
+JSClass("DBObjectDatabase", JSObject, {
+
+    initWithURL: function(url, fileManager){
+        this.url = url;
+        this.fileManager = fileManager;
+    },
+
+    id: function(table){
+        var chunks = Array.prototype.slice.call(arguments, 1);
+        var hash = new JSSHA1Hash();
+        hash.start();
+        for (var i = 0, l = chunks.length; i < l; ++i){
+            hash.add(chunks[i]);
+        }
+        if (i === 0){
+            hash.add(new UUID().bytes);
+        }
+        hash.finish();
+        var hex = hash.digest().hexStringRepresentation();
+        return table + '_' + hex;
+    },
+
+    object: function(id, completion, target){
+        if (!completion){
+            completion = Promise.completion();
+        }
+        var url = this._urlForId(id);
+        this.fileManager.contentsAtURL(url, function(contents){
+            if (contents === null){
+                completion.call(target, null);
+                return;
+            }
+            var obj = null;
+            try{
+                var json = contents.stringByDecodingUTF8();
+                obj = JSON.parse(json);
+            }catch (e){
+                completion.call(target, null);
+                return;
+            }
+            completion.call(target, obj);
+        }, this);
+        return completion.promise;
+    },
+
+    saveObject: function(obj, completion, target){
+        if (!completion){
+            completion = Promise.completion(Promise.resolveTrue);
+        }
+        var url = this._urlForId(obj.id);
+        var json = JSON.stringify(obj);
+        var contents = json.utf8();
+        this.fileManager.createFileAtURL(url, contents, function(success){
+            completion.call(target, success);
+        }, this);
+        return completion.promise;
+    },
+
+    deleteObject: function(id, completion, target){
+        if (!completion){
+            completion = Promise.completion(Promise.resolveTrue);
+        }
+        var url = this._urlForId(id);
+        this.fileManager.removeItemAtURL(url, function(success){
+            completion.call(target, success);
+        }, this);
+        return completion.promise;
+    },
+
+    _urlForId: function(id){
+        var hashlen = 40;
+        var i = id.length - hashlen;
+        var table = id.substr(0, i - 1);
+        var hash1 = id.substr(i, 2);
+        var hash2 = id.substr(i + 2, 2);
+        var final = id.substr(i + 4);
+        var components = [
+            table,
+            hash1,
+            hash2,
+            final
+        ];
+        return this.url.appendingPathComponents(components);
+    }
+
+});
---
+++ Tests/DBKitTests/DBObjectDatabaseTests.js
@@ -0,0 +1,127 @@
+// #import DBKit
+// #import TestKit
+// #import Hash
+/* global JSClass, TKTestSuite, DBObjectDatabase, JSFileManager, TKExpectation */
+/* global TKAssert, TKAssertEquals, TKAssertNotEquals, TKAssertFloatEquals, TKAssertExactEquals, TKAssertNotExactEquals, TKAssertObjectEquals, TKAssertObjectNotEquals, TKAssertNotNull, TKAssertNull, TKAssertUndefined, TKAssertNotUndefined, TKAssertThrows, TKAssertLessThan, TKAssertLessThanOrEquals, TKAssertGreaterThan, TKAssertGreaterThanOrEquals, TKAssertArrayEquals, JSSHA1Hash */
+'use strict';
+
+JSClass("DBObjectDatabaseTests", TKTestSuite, {
+
+    manager: null,
+    db: null,
+
+    setup: function(){
+        var timestamp = (new Date()).getTime();
+        this.manager = JSFileManager.initWithIdentifier("io.breakside.JSKit.FoundationTests-%d".sprintf(timestamp));
+        var expectation = TKExpectation.init();
+        expectation.call(this.manager.open, this.manager, function(state){
+            TKAssertExactEquals(state, JSFileManager.State.success);
+            var url = this.manager.temporaryDirectoryURL.appendingPathComponent('DBObjectDatabaseTests', true);
+            this.db = DBObjectDatabase.initWithURL(url, this.manager);
+        }, this);
+        this.wait(expectation, 1.0);
+    },
+
+    teardown: function(){
+        var expectation = TKExpectation.init();
+        expectation.call(this.manager.destroy, this.manager, function(success){
+            TKAssert(success);
+        }, this);
+        this.wait(expectation, 2.0);
+    },
+
+    testId: function(){
+        var id = this.db.id("testing");
+        TKAssertEquals(id.length, 48);
+        TKAssertEquals(id.substr(0, 8), "testing_");
+
+        var id2 = this.db.id("testing");
+        TKAssertEquals(id.length, 48);
+        TKAssertEquals(id.substr(0, 8), "testing_");
+        TKAssertNotEquals(id, id2);
+
+        var data = "Hello, World!".utf8();
+        id = this.db.id("test", data);
+        var hash = JSSHA1Hash(data).hexStringRepresentation();
+        var expected = "test_" + hash;
+        TKAssertEquals(id, expected);
+        id2 = this.db.id("test", data);
+        TKAssertEquals(id, id2);
+    },
+
+    testSave: function(){
+        var obj = {
+            id: this.db.id("test"),
+            one: 1,
+            two: "2",
+            three: null
+        };
+        var expectation = TKExpectation.init();
+        expectation.call(this.db.saveObject, this.db, obj, function(success){
+            TKAssert(success);
+        });
+        this.wait(expectation, 1.0);
+    },
+
+    testObject: function(){
+        var id = this.db.id("test");
+        var obj = {
+            id: id,
+            one: 1,
+            two: "2",
+            three: null
+        };
+        var expectation = TKExpectation.init();
+        expectation.call(this.db.saveObject, this.db, obj, function(success){
+            TKAssert(success);
+            expectation.call(this.db.object, this.db, id, function(obj){
+                TKAssertNotNull(obj);
+                TKAssertEquals(obj.id, id);
+                TKAssertExactEquals(obj.one, 1);
+                TKAssertExactEquals(obj.two, "2");
+                TKAssertNull(obj.three);
+            }, this);
+        }, this);
+        this.wait(expectation, 1.0);
+    },
+
+    testMissingObject: function(){
+        var id = this.db.id("test");
+        var obj = {
+            id: id,
+            one: 1,
+            two: "2",
+            three: null
+        };
+        var expectation = TKExpectation.init();
+        expectation.call(this.db.object, this.db, id, function(obj){
+            TKAssertNull(obj);
+        });
+        this.wait(expectation, 1.0);
+    },
+
+    testDeleteObject: function(){
+        var id = this.db.id("test");
+        var obj = {
+            id: id,
+            one: 1,
+            two: "2",
+            three: null
+        };
+        var expectation = TKExpectation.init();
+        expectation.call(this.db.saveObject, this.db, obj, function(success){
+            TKAssert(success);
+            expectation.call(this.db.object, this.db, id, function(obj){
+                TKAssertNotNull(obj);
+                expectation.call(this.db.deleteObject, this.db, id, function(success){
+                    TKAssert(success);
+                    expectation.call(this.db.object, this.db, id, function(obj){
+                        TKAssertNull(obj);
+                    }, this);
+                }, this);
+            }, this);
+        }, this);
+        this.wait(expectation, 1.0);
+    }
+
+});
