13d8d61d57346da328f389d8e10eecf6c5ca54c6
Owen Shaw <owen@owenshaw.com>
2019-04-13 12:12:40 -0700

    make a leading path be a positional argument

--- Frameworks/UIKit/UIApplication+HTML.js
+++ Frameworks/UIKit/UIApplication+HTML.js
@@ -9,12 +9,15 @@ UIApplication.definePropertiesFromExtensions({
         var url = JSURL.initWithString(window.location.href);
         var fragment = url.fragment;
         var query = "";
+        var positional = [];
         if (fragment !== null && fragment.length > 0){
             if (fragment[0] == '/'){
                 var queryIndex = fragment.indexOf('?');
                 if (queryIndex >= 0){
                     query = fragment.substr(queryIndex + 1);
-                    query += "state=" + fragment.substr(0, queryIndex);
+                    positional.push(fragment.substr(0, queryIndex));
+                }else{
+                    positional.push(fragment);
                 }
             }else{
                 query = fragment;
@@ -23,6 +26,8 @@ UIApplication.definePropertiesFromExtensions({
         var options = this.bundle.info.UIApplicationLaunchOptions || {};
         var args = JSArguments.initWithOptions(options);
         args.parseQueryString(query);
+        url.fragment = null;
+        window.history.replaceState(null, null, url.encodedString);
         return args;
     },
 
