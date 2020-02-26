e17a95d031bd8242870d7e1282b5b02b8e070c2d
Owen Shaw <owen@owenshaw.com>
2020-02-26 15:29:11 -0800

    Update license

--- Frameworks/Foundation/UUID.js
+++ Frameworks/Foundation/UUID.js
@@ -1,3 +1,18 @@
+// Copyright 2020 Breakside Inc.
+//
+// Licensed under the Breakside Public License, Version 1.0 (the "License");
+// you may not use this file except in compliance with the License.
+// If a copy of the License was not distributed with this file, you may
+// obtain a copy at
+//
+//     http://breakside.io/licenses/LICENSE-1.0.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // #feature Uint8Array
 // #import "Uint8Array+JS.js"
 // #import "String+JS.js"
