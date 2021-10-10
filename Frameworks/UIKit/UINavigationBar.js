30d45aa4fdf11534f1e380d829839da786449bbb
Owen Shaw <owen@owenshaw.com>
2021-10-09 17:38:45 -0700

    Change default nav controller animation timing

--- Frameworks/UIKit/UINavigationBar.js
+++ Frameworks/UIKit/UINavigationBar.js
@@ -90,11 +90,11 @@ JSClass("UINavigationBar", UIView, {
     pushAnimator: null,
 
     createPushAnimator: function(){
-        return UIViewPropertyAnimator.initWithDuration(0.2, UIAnimation.Timing.easeOut);
+        return UIViewPropertyAnimator.initWithDuration(0.2, UIAnimation.Timing.easeInOut);
     },
 
     createPopAnimator: function(){
-        return UIViewPropertyAnimator.initWithDuration(0.2, UIAnimation.Timing.easeOut);
+        return UIViewPropertyAnimator.initWithDuration(0.2, UIAnimation.Timing.easeInOut);
     },
 
     pushItem: function(item, animated){
