238167aaef56b8edb7a1f6188fdab4858da5b180
Owen Shaw <owen@owenshaw.com>
2022-01-26 16:02:06 -0800

    fix comment prefix

--- Frameworks/ImageKit/IKMatrix.js
+++ Frameworks/ImageKit/IKMatrix.js
@@ -1,4 +1,4 @@
-// // Copyright 2022 Breakside Inc.
+// Copyright 2022 Breakside Inc.
 //
 // Licensed under the Breakside Public License, Version 1.0 (the "License");
 // you may not use this file except in compliance with the License.
--- Frameworks/TestKit/TKMock.js
+++ Frameworks/TestKit/TKMock.js
@@ -1,4 +1,4 @@
-// // Copyright 2021 Breakside Inc.
+// Copyright 2021 Breakside Inc.
 //
 // Licensed under the Breakside Public License, Version 1.0 (the "License");
 // you may not use this file except in compliance with the License.
