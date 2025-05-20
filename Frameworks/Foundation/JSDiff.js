596d0f24b10764dd2ec63a20fc48271e32328389
Owen Shaw <owen@owenshaw.com>
2025-05-19 17:16:06 -0700

    use better diff algorithm

--- Frameworks/Foundation/JSDiff.js
+++ Frameworks/Foundation/JSDiff.js
@@ -20,80 +20,175 @@ JSClass("JSDiff", JSObject, {
         this.initWithArrays(a, b);
     },
 
+    operations: JSLazyInitProperty(function(){
+        return this.shortestOperationList();
+    }),
+
     commonIndexes: JSLazyInitProperty(function(){
+        var operations = this.operations;
+        var operationIndex = 0;
+        var operationCount = operations.length;
+        var m = this.a.length;
+        var n = this.b.length;
+        var commonIndexes = [];
+        var i = 0;
+        var j = 0;
+        while (i < m && j < n){
+            if (operationIndex < operationCount && operations[operationIndex].originalIndex === i){
+                if (operations[operationIndex].operator === JSDiff.Operator.delete){
+                    ++i;
+                }else{
+                    ++j;
+                }
+                ++operationIndex;
+            }else{
+                commonIndexes.push([i, j]);
+                ++i;
+                ++j;
+            }
+        }
+        return commonIndexes;
+    }),
+
+    shortestOperationList: function(){
+        var operations = [];
         var a = this.a;
         var b = this.b;
         if (this.mapFunction !== null){
             a = a.map(this.mapFunction);
             b = b.map(this.mapFunction);
         }
-        return this.indexesOfLongestCommonSubsequence(a, 0, a.length - 1, b, 0, b.length - 1);
-    }),
+        var m = a.length;
+        var n = b.length;
+        var i = 0;
+        var j = 0;
 
-    recursionCount: 0,
+        // common prefix
+        for (; i < m && j < n; ++i, ++j){
+            ++this.comparisonCount;
+            if (a[i] !== b[j]){
+                break;
+            }
+        }
+
+        // common suffix
+        for (; m > i && n > j; --m, --n){
+            ++this.comparisonCount;
+            if (a[m - 1] !== b[n - 1]){
+                break;
+            }
+        }
+
+        // special case, a and b are equal
+        if (i === m && j === n){
+            return operations;
+        }
+
+        // special case, b contains all of a plus extra at the end
+        if (i === m){
+            for (; j < n; ++j){
+                operations.push(JSDiffOperation(JSDiff.Operator.insert, i, j));
+            }
+            return operations;
+        }
+
+        // special case, b contains only the start of a
+        if (j === n){
+            for (; i < m; ++i){
+                operations.push(JSDiffOperation(JSDiff.Operator.delete, i, j));
+            }
+            return operations;
+        }
+
+        var operationCount = 1;
+        var maximumOperationCount = Math.min(m + n, this.maximumOperationCount);
+        var diagonalNumber;
+        var minimumDiagonalNumber = -1;
+        var maximumDiagonalNumber = 1;
+        var diagonals = {};
+        var diagonal;
+        var referenceDiagonal;
+        var operation;
+        var operator;
+        diagonals[0] = {
+            i: i,
+            operation: null
+        };
+        for (; operationCount <= maximumOperationCount; ++operationCount){
+            for (diagonalNumber = minimumDiagonalNumber; diagonalNumber <= maximumDiagonalNumber; diagonalNumber += 2){
+                ++this.comparisonCount;
+                if (diagonalNumber === -operationCount || (diagonalNumber !== operationCount && diagonals[diagonalNumber + 1].i >= diagonals[diagonalNumber - 1].i)){
+                    referenceDiagonal = diagonals[diagonalNumber + 1];
+                    i = referenceDiagonal.i + 1;
+                    j = i + diagonalNumber;
+                    operation = JSDiffOperation(JSDiff.Operator.delete, i - 1, j);
+                }else{
+                    referenceDiagonal = diagonals[diagonalNumber - 1];
+                    i = referenceDiagonal.i;
+                    j = i + diagonalNumber;
+                    operation = JSDiffOperation(JSDiff.Operator.insert, i, j - 1);
+                }
+                operation.previous = referenceDiagonal.operation;
+                for (; i < m && j < n; ++i, ++j){
+                    ++this.comparisonCount;
+                    if (a[i] !== b[j]){
+                        break;
+                    }
+                }
+                diagonal = diagonals[diagonalNumber];
+                if (diagonal === undefined){
+                    diagonal = {
+                        i: i,
+                        operation: operation,
+                    };
+                    diagonals[diagonalNumber] = diagonal;
+                }else{
+                    diagonal.i = i;
+                    diagonal.operation = operation;
+                }
+                if (i === m && j === n){
+                    while (operation !== null){
+                        operations.unshift(operation);
+                        operation = operation.previous;
+                    }
+                    return operations;
+                }
+                if (i === m){
+                    minimumDiagonalNumber = diagonalNumber + 2;
+                }
+                if (j === n){
+                    maximumDiagonalNumber = diagonalNumber - 2;
+                }
+            }
+            --minimumDiagonalNumber;
+            ++maximumDiagonalNumber;
+        }
+
+        return null;
+    },
+
+    maximumOperationCount: Infinity,
     comparisonCount: 0,
-
-    indexesOfLongestCommonSubsequence: function(a, i0, i1, b, j0, j1){
-        ++this.recursionCount;
-        if (i1 >= i0 && j1 >= j0){
-            ++this.comparisonCount;
-            if (a[i0] === b[j0]){
-                var head = [];
-                ++this.comparisonCount;
-                while (i1 >= i0 && j1 >= j0 && a[i0] === b[i0]){
-                    head.push([i0, j0]);
-                    ++i0;
-                    ++j0;
-                    ++this.comparisonCount;
-                }
-                return head.concat(this.indexesOfLongestCommonSubsequence(a, i0, i1, b, j0, j1));
-            }
-            ++this.comparisonCount;
-            if (a[i1] === b[j1]){
-                var tail = [];
-                ++this.comparisonCount;
-                while (i1 >= i0 && j1 >= j0 && a[i1] === b[j1]){
-                    tail.unshift([i1, j1]);
-                    --i1;
-                    --j1;
-                    ++this.comparisonCount;
-                }
-                return this.indexesOfLongestCommonSubsequence(a, i0, i1, b, j0, j1).concat(tail);
-            }
-            var sequence1 = this.indexesOfLongestCommonSubsequence(a, i0, i1, b, j0, j1 - 1);
-            var sequence2 = this.indexesOfLongestCommonSubsequence(a, i0, i1 - 1, b, j0, j1);
-            if (sequence1.length >= sequence2.length){
-                return sequence1;
-            }
-            return sequence2;
-        }
-        return [];
-    },
-
-    operations: function(){
-        var a = [];
-        var b = [];
-        var i, l;
-        var j, k;
-        var commonIndexes = this.commonIndexes;
-        for (i = 0, l = a.length; i < l; ++i){
-        }
-        return [a, b];
-    },
-
-    longestCommonSubsequence: function(){
-        var commonIndexes = this.commonIndexes;
-        var sequence = [];
-        var i, l;
-        for (i = 0, l = commonIndexes.length; i < l; ++i){
-            sequence.push(this.a[commonIndexes[i][0]]);
-        }
-        return sequence;
-    }
 
 });
 
-JSDiff.Operation = {
+JSGlobalObject.JSDiffOperation = function(operator, originalIndex, modifiedIndex){
+    if (this === undefined){
+        return new JSDiffOperation(operator, originalIndex, modifiedIndex);
+    }
+    if (operator instanceof JSDiffOperation){
+        this.operator = operator.operator;
+        this.originalIndex = operator.originalIndex;
+        this.modifiedIndex = operator.modifiedIndex;
+    }else{
+        this.operator = operator;
+        this.originalIndex = originalIndex;
+        this.modifiedIndex = modifiedIndex;
+    }
+    this.previous = null;
+};
+
+JSDiff.Operator = {
     insert: 1,
     delete: 2
 };
--- Tests/FoundationTests/JSDiffTests.js
+++ Tests/FoundationTests/JSDiffTests.js
@@ -4,10 +4,41 @@
 
 JSClass("JSDiffTests", TKTestSuite, {
 
-    testCommonIndexes: function(){
-        // Insert at end
-        var diff = JSDiff.initWithStrings("ABCDEF", "ABCDEFG");
+    testOperationsAndCommonIndexes: function(){
+        // Equal
+        var diff = JSDiff.initWithStrings("ABCDEF", "ABCDEF");
+        var operations = diff.operations;
+        TKAssertEquals(operations.length, 0);
         var indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 6);
+        TKAssertEquals(indexes[0][0], 0);
+        TKAssertEquals(indexes[0][1], 0);
+        TKAssertEquals(indexes[1][0], 1);
+        TKAssertEquals(indexes[1][1], 1);
+        TKAssertEquals(indexes[2][0], 2);
+        TKAssertEquals(indexes[2][1], 2);
+        TKAssertEquals(indexes[3][0], 3);
+        TKAssertEquals(indexes[3][1], 3);
+        TKAssertEquals(indexes[4][0], 4);
+        TKAssertEquals(indexes[4][1], 4);
+        TKAssertEquals(indexes[5][0], 5);
+        TKAssertEquals(indexes[5][1], 5);
+
+        // Empty
+        diff = JSDiff.initWithStrings("", "");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 0);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 0);
+
+        // Insert at end
+        diff = JSDiff.initWithStrings("ABCDEF", "ABCDEFG");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[0].originalIndex, 6);
+        TKAssertEquals(operations[0].modifiedIndex, 6);
+        indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 6);
         TKAssertEquals(indexes[0][0], 0);
         TKAssertEquals(indexes[0][1], 0);
@@ -24,6 +55,11 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // Insert at start
         diff = JSDiff.initWithStrings("ABCDEF", "0ABCDEF");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[0].originalIndex, 0);
+        TKAssertEquals(operations[0].modifiedIndex, 0);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 6);
         TKAssertEquals(indexes[0][0], 0);
@@ -41,6 +77,11 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // Insert in middle
         diff = JSDiff.initWithStrings("ABCDEF", "ABC-DEF");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[0].originalIndex, 3);
+        TKAssertEquals(operations[0].modifiedIndex, 3);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 6);
         TKAssertEquals(indexes[0][0], 0);
@@ -58,6 +99,11 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // Delete from start
         diff = JSDiff.initWithStrings("ABCDEF", "BCDEF");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[0].originalIndex, 0);
+        TKAssertEquals(operations[0].modifiedIndex, 0);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 5);
         TKAssertEquals(indexes[0][0], 1);
@@ -73,6 +119,11 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // Delete from end
         diff = JSDiff.initWithStrings("ABCDEF", "ABCDE");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[0].originalIndex, 5);
+        TKAssertEquals(operations[0].modifiedIndex, 5);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 5);
         TKAssertEquals(indexes[0][0], 0);
@@ -88,6 +139,11 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // Delete from middle
         diff = JSDiff.initWithStrings("ABCDEF", "ABDEF");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[0].originalIndex, 2);
+        TKAssertEquals(operations[0].modifiedIndex, 2);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 5);
         TKAssertEquals(indexes[0][0], 0);
@@ -103,6 +159,14 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // move from start to end
         diff = JSDiff.initWithStrings("ABCDEF", "BCDEFA");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 2);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[0].originalIndex, 0);
+        TKAssertEquals(operations[0].modifiedIndex, 0);
+        TKAssertEquals(operations[1].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[1].originalIndex, 6);
+        TKAssertEquals(operations[1].modifiedIndex, 5);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 5);
         TKAssertEquals(indexes[0][0], 1);
@@ -118,6 +182,14 @@ JSClass("JSDiffTests", TKTestSuite, {
 
         // delete and insert
         diff = JSDiff.initWithStrings("ABCDEF", "ACDEFG");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 2);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[0].originalIndex, 1);
+        TKAssertEquals(operations[0].modifiedIndex, 1);
+        TKAssertEquals(operations[1].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[1].originalIndex, 6);
+        TKAssertEquals(operations[1].modifiedIndex, 5);
         indexes = diff.commonIndexes;
         TKAssertEquals(indexes.length, 5);
         TKAssertEquals(indexes[0][0], 0);
@@ -130,53 +202,257 @@ JSClass("JSDiffTests", TKTestSuite, {
         TKAssertEquals(indexes[3][1], 3);
         TKAssertEquals(indexes[4][0], 5);
         TKAssertEquals(indexes[4][1], 4);
+
+        // paper
+        diff = JSDiff.initWithStrings("axxbxx", "bxxaxxbxx");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 3);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[0].originalIndex, 0);
+        TKAssertEquals(operations[0].modifiedIndex, 0);
+        TKAssertEquals(operations[1].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[1].originalIndex, 0);
+        TKAssertEquals(operations[1].modifiedIndex, 1);
+        TKAssertEquals(operations[2].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[2].originalIndex, 0);
+        TKAssertEquals(operations[2].modifiedIndex, 2);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 6);
+        TKAssertEquals(indexes[0][0], 0);
+        TKAssertEquals(indexes[0][1], 3);
+        TKAssertEquals(indexes[1][0], 1);
+        TKAssertEquals(indexes[1][1], 4);
+        TKAssertEquals(indexes[2][0], 2);
+        TKAssertEquals(indexes[2][1], 5);
+        TKAssertEquals(indexes[3][0], 3);
+        TKAssertEquals(indexes[3][1], 6);
+        TKAssertEquals(indexes[4][0], 4);
+        TKAssertEquals(indexes[4][1], 7);
+        TKAssertEquals(indexes[5][0], 5);
+        TKAssertEquals(indexes[5][1], 8);
+
+        // paper
+        diff = JSDiff.initWithStrings("abcab", "cbab");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 3);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[0].originalIndex, 0);
+        TKAssertEquals(operations[0].modifiedIndex, 0);
+        TKAssertEquals(operations[1].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[1].originalIndex, 1);
+        TKAssertEquals(operations[1].modifiedIndex, 0);
+        TKAssertEquals(operations[2].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[2].originalIndex, 3);
+        TKAssertEquals(operations[2].modifiedIndex, 1);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 3);
+        TKAssertEquals(indexes[0][0], 2);
+        TKAssertEquals(indexes[0][1], 0);
+        TKAssertEquals(indexes[1][0], 3);
+        TKAssertEquals(indexes[1][1], 2);
+        TKAssertEquals(indexes[2][0], 4);
+        TKAssertEquals(indexes[2][1], 3);
+
+        // wiki
+        diff = JSDiff.initWithStrings("abcdfghjqz", "abcdefgijkrxyz");
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 8);
+        TKAssertEquals(operations[0].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[0].originalIndex, 4);
+        TKAssertEquals(operations[0].modifiedIndex, 4);
+        TKAssertEquals(operations[1].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[1].originalIndex, 6);
+        TKAssertEquals(operations[1].modifiedIndex, 7);
+        TKAssertEquals(operations[2].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[2].originalIndex, 7);
+        TKAssertEquals(operations[2].modifiedIndex, 7);
+        TKAssertEquals(operations[3].operator, JSDiff.Operator.delete);
+        TKAssertEquals(operations[3].originalIndex, 8);
+        TKAssertEquals(operations[3].modifiedIndex, 9);
+        TKAssertEquals(operations[4].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[4].originalIndex, 9);
+        TKAssertEquals(operations[4].modifiedIndex, 9);
+        TKAssertEquals(operations[5].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[5].originalIndex, 9);
+        TKAssertEquals(operations[5].modifiedIndex, 10);
+        TKAssertEquals(operations[6].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[6].originalIndex, 9);
+        TKAssertEquals(operations[6].modifiedIndex, 11);
+        TKAssertEquals(operations[7].operator, JSDiff.Operator.insert);
+        TKAssertEquals(operations[7].originalIndex, 9);
+        TKAssertEquals(operations[7].modifiedIndex, 12);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 8);
+        TKAssertEquals(indexes[0][0], 0);
+        TKAssertEquals(indexes[0][1], 0);
+        TKAssertEquals(indexes[1][0], 1);
+        TKAssertEquals(indexes[1][1], 1);
+        TKAssertEquals(indexes[2][0], 2);
+        TKAssertEquals(indexes[2][1], 2);
+        TKAssertEquals(indexes[3][0], 3);
+        TKAssertEquals(indexes[3][1], 3);
+        TKAssertEquals(indexes[4][0], 4);
+        TKAssertEquals(indexes[4][1], 5);
+        TKAssertEquals(indexes[5][0], 5);
+        TKAssertEquals(indexes[5][1], 6);
+        TKAssertEquals(indexes[6][0], 7);
+        TKAssertEquals(indexes[6][1], 8);
+        TKAssertEquals(indexes[7][0], 9);
+        TKAssertEquals(indexes[7][1], 13);
     },
 
     testPerformance: function(){
-        var a = [];
-        var b = [];
+        var N = [];
+        var a, b;
         var i;
         var n = 1000;
         for (i = 0; i < n; ++i){
-            a.push(i);
-            b.push(i);
+            N.push(i);
         }
 
+        var diff, operations, indexes;
+
+        // Equal
+        a = JSCopy(N);
+        b = JSCopy(N);
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 0);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, a.length);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length);
+
+        // Empty empty
+        a = [];
+        b = [];
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 0);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 0);
+        TKAssertLessThanOrEquals(diff.comparisonCount, 0);
+
+        // Add to empty
+        a = [];
+        b = JSCopy(N);
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, b.length);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 0);
+        TKAssertLessThanOrEquals(diff.comparisonCount, 0);
+
+        // Remove all
+        a = JSCopy(N);
+        b = [];
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, a.length);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 0);
+        TKAssertLessThanOrEquals(diff.comparisonCount, 0);
+
         // insert at end
+        a = JSCopy(N);
+        b = JSCopy(N);
         b.push(n);
-        var diff = JSDiff.initWithArrays(a, b);
-        var indexes = diff.commonIndexes;
-        TKAssertEquals(indexes.length, n);
-        TKAssertLessThanOrEquals(diff.comparisonCount, n + 2);
-        TKAssertLessThanOrEquals(diff.recursionCount, 2);
-        b.pop();
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, a.length);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length);
 
         // delete from end
-        a.push(n);
+        a = JSCopy(N);
+        b = JSCopy(N);
+        b.pop();
         diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
         indexes = diff.commonIndexes;
-        TKAssertEquals(indexes.length, n);
-        TKAssertLessThanOrEquals(diff.comparisonCount, n + 2);
-        TKAssertLessThanOrEquals(diff.recursionCount, 2);
-        a.pop();
+        TKAssertEquals(indexes.length, b.length);
+        TKAssertLessThanOrEquals(diff.comparisonCount, b.length);
 
         // insert at start
+        a = JSCopy(N);
+        b = JSCopy(N);
         b.unshift(-1);
         diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
         indexes = diff.commonIndexes;
-        TKAssertEquals(indexes.length, n);
-        TKAssertLessThanOrEquals(diff.comparisonCount, n + 3);
-        TKAssertLessThanOrEquals(diff.recursionCount, 2);
-        b.shift();
+        TKAssertEquals(indexes.length, a.length);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length + 1);
 
         // delete from start
-        a.unshift(-1);
+        a = JSCopy(N);
+        b = JSCopy(N);
+        b.shift();
         diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 1);
         indexes = diff.commonIndexes;
-        TKAssertEquals(indexes.length, n);
-        TKAssertLessThanOrEquals(diff.comparisonCount, n + 3);
-        TKAssertLessThanOrEquals(diff.recursionCount, 2);
-        a.shift();
+        TKAssertEquals(indexes.length, b.length);
+        TKAssertLessThanOrEquals(diff.comparisonCount, b.length + 1);
+
+        // move from start to end
+        a = JSCopy(N);
+        b = JSCopy(N);
+        b.push(b.shift());
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 2);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, a.length - 1);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length + 5);
+
+        // move from end to start
+        a = JSCopy(N);
+        b = JSCopy(N);
+        b.unshift(b.pop());
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 2);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, a.length - 1);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length + 7);
+
+        // move from start to middle
+        a = JSCopy(N);
+        b = JSCopy(N);
+        b.splice(Math.floor(n / 2), 0, b.shift());
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 2);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, a.length - 1);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length + 5);
+
+        // move from end to middle
+        a = JSCopy(N);
+        b = JSCopy(N);
+        b.splice(Math.floor(n / 2), 0, b.pop());
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, 2);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, a.length - 1);
+        TKAssertLessThanOrEquals(diff.comparisonCount, a.length + 7);
+
+        // totally different
+        a = JSCopy(N);
+        b = [];
+        for (i = 0; i < n; ++i){
+            b.push(n + i);
+        }
+        diff = JSDiff.initWithArrays(a, b);
+        operations = diff.operations;
+        TKAssertEquals(operations.length, a.length + b.length);
+        indexes = diff.commonIndexes;
+        TKAssertEquals(indexes.length, 0);
+        TKAssertLessThanOrEquals(diff.comparisonCount, 2 * (a.length * b.length + operations.length));
+
     }
 
 });
